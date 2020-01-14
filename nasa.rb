d636c107c4e01e2feb255475b6a83eef7fc75d67
alexcct <ivanalejandro@MacBook-Air-de-Ivan.local>
2020-01-14 15:13:25 -0300

    first repository

--- nasa.rb
+++ nasa.rb
@@ -4,7 +4,10 @@ require "uri"
 require "net/https"
 require 'json'
 
-url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=c3vYTk5lHrjjBLR04kaWLThlsipRNGnIJ1VYg7qh")
+
+def request(url)
+url = URI(url)
+
 
 https = Net::HTTP.new(url.host, url.port);
 https.use_ssl = true
@@ -12,8 +15,36 @@ https.use_ssl = true
 request = Net::HTTP::Get.new(url)
 
 response = https.request(request)
-body = JSON.parse (response.read_body)
-puts body
+JSON.parse (response.read_body)
+end
+
+body = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=c3vYTk5lHrjjBLR04kaWLThlsipRNGnIJ1VYg7qh")
+
+
+a = body.values
 
 
 
+File.open('nasa.html','w') do |f|
+f.puts "<!DOCTYPE html>\n
+   <html lang='en'>\n
+	<head>\n
+		<meta charset='UTF-8'>\n
+		<meta name='viewport' content='width=device-width, initial-scale=1.0'>\n
+		<meta http-equiv='X-UA-Compatible' content='ie=edge'>\n
+		<title>Document</title>\n
+		</head>\n
+	<body>\n
+	<ul>"
+	for j in 0..a[0].length
+	a[0][j].each do |k,v|
+	f.puts " <li><img src='#{v}'></li>" if k=="img_src"
+	
+	end
+	end
+	
+	f.puts "</ul>\n
+		<body>\n
+		</html>"
+	
+	end
